8f3e21a21ecec9d23bba1e03cadd2eb91a911121
code1009 <code1009@naver.com>
2024-11-25 14:13:40 +0900

    1

--- Application/AppWindow/MainFrame.cpp
+++ Application/AppWindow/MainFrame.cpp
@@ -19,6 +19,326 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
+VSplitter::VSplitter()
+{
+	_Color = GetSysColor(COLOR_BTNFACE);
+	_Percent = 0.8;
+}
+
+void VSplitter::Draw(HWND hWnd, int curPos)
+{
+	const HDC hdc = GetDC(hWnd);
+
+
+	if (nullptr == hdc)
+	{
+		return;
+	}
+
+
+	const HBRUSH hBrush = CreateSolidBrush(_Color);
+	const HPEN   hPen = CreatePen(PS_SOLID, 1, _Color);
+
+
+	if (nullptr == hBrush)
+	{
+		ReleaseDC(hWnd, hdc);
+		return;
+	}
+	if (nullptr == hPen)
+	{
+		DeleteObject(hBrush);
+		ReleaseDC(hWnd, hdc);
+		return;
+	}
+
+
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	RECT barRect;
+	barRect.left = 0;
+	barRect.top = curPos - _Width / 2;
+	barRect.right = rect.right;
+	barRect.bottom = curPos + _Width / 2;
+
+
+	//SetROP2(hdc, R2_NOT);
+	//SetBkMode(hdc, TRANSPARENT);
+
+	const HGDIOBJ hOldBrush = SelectObject(hdc, hBrush);
+	const HGDIOBJ hOldPen = SelectObject(hdc, hPen);
+
+	Rectangle(hdc, barRect.left, barRect.top, barRect.right, barRect.bottom);
+
+	SelectObject(hdc, hOldBrush);
+	SelectObject(hdc, hOldPen);
+
+	DeleteObject(hBrush);
+	DeleteObject(hPen);
+
+	ReleaseDC(hWnd, hdc);
+}
+
+void VSplitter::UpdateLayout(HWND hWnd, HWND hFirstWnd, HWND hSecondWnd)
+{
+	//-----------------------------------------------------------------------
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	//-----------------------------------------------------------------------
+	UINT cx;
+	UINT cy;
+	cx = static_cast<UINT>(rect.right - rect.left);
+	cy = static_cast<UINT>(rect.bottom - rect.top);
+
+
+	//-----------------------------------------------------------------------
+	int first;
+	int second;
+
+
+	first = static_cast<int>((cy * _Percent) - (_Width / 2));
+	second = static_cast<int>(cy - (cy * _Percent) - (_Width / 2));
+
+	::MoveWindow(hFirstWnd, 0, 0, cx, first, TRUE);
+	::MoveWindow(hSecondWnd, 0, first + _Width, cx, second, TRUE);
+}
+
+void VSplitter::onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	SetCapture(hWnd);
+	_OldPos = -1;
+}
+
+void VSplitter::onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (GetCapture() != hWnd)
+	{
+		return;
+	}
+
+	ReleaseCapture();
+
+	if (_OldPos > 0)
+	{
+		Draw(hWnd, _OldPos);
+	}
+
+	SendMessage(hWnd, WM_SIZE, 0, 0);
+}
+
+void VSplitter::onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (GetCapture() != hWnd)
+	{
+		return;
+	}
+
+
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	const int posValue = GET_Y_LPARAM(lParam);
+	const int maxValue = rect.bottom;
+	_Percent = static_cast<double>(posValue) / static_cast<double>(maxValue);
+
+
+	if (_Percent < 0.1)
+	{
+		_Percent = 0.1;
+	}
+	else if (_Percent > 0.9)
+	{
+		_Percent = 0.9;
+	}
+
+
+	const int curValue = static_cast<int>(maxValue * _Percent);
+	if (_OldPos != curValue)
+	{
+		if (_OldPos > 0)
+		{
+			Draw(hWnd, _OldPos);
+		}
+
+		Draw(hWnd, curValue);
+		_OldPos = curValue;
+	}
+
+
+	SendMessage(hWnd, WM_SIZE, 0, 0);
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+HSplitter::HSplitter()
+{
+	_Color = GetSysColor(COLOR_BTNFACE);
+	_Percent = 0.8;
+}
+
+void HSplitter::Draw(HWND hWnd, int curPos)
+{
+	const HDC hdc = GetDC(hWnd);
+
+
+	if (nullptr == hdc)
+	{
+		return;
+	}
+
+
+	const HBRUSH hBrush = CreateSolidBrush(_Color);
+	const HPEN   hPen = CreatePen(PS_SOLID, 1, _Color);
+
+
+	if (nullptr == hBrush)
+	{
+		ReleaseDC(hWnd, hdc);
+		return;
+	}
+	if (nullptr == hPen)
+	{
+		DeleteObject(hBrush);
+		ReleaseDC(hWnd, hdc);
+		return;
+	}
+
+
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	RECT barRect;
+	barRect.top = 0;
+	barRect.left = curPos - _Width / 2;
+	barRect.bottom = rect.right;
+	barRect.right = curPos + _Width / 2;
+
+
+	//SetROP2(hdc, R2_NOT);
+	//SetBkMode(hdc, TRANSPARENT);
+
+	const HGDIOBJ hOldBrush = SelectObject(hdc, hBrush);
+	const HGDIOBJ hOldPen = SelectObject(hdc, hPen);
+
+	Rectangle(hdc, barRect.left, barRect.top, barRect.right, barRect.bottom);
+
+	SelectObject(hdc, hOldBrush);
+	SelectObject(hdc, hOldPen);
+
+	DeleteObject(hBrush);
+	DeleteObject(hPen);
+
+	ReleaseDC(hWnd, hdc);
+}
+
+void HSplitter::UpdateLayout(HWND hWnd, HWND hFirstWnd, HWND hSecondWnd)
+{
+	//-----------------------------------------------------------------------
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	//-----------------------------------------------------------------------
+	UINT cx;
+	UINT cy;
+	cx = static_cast<UINT>(rect.right - rect.left);
+	cy = static_cast<UINT>(rect.bottom - rect.top);
+
+
+	//-----------------------------------------------------------------------
+	int first;
+	int second;
+
+
+	first = static_cast<int>((cx * _Percent) - (_Width / 2));
+	second = static_cast<int>(cx - (cx * _Percent) - (_Width / 2));
+
+	::MoveWindow(hFirstWnd, 0, 0, first, cy, TRUE);
+	::MoveWindow(hSecondWnd, first + _Width, 0, second, cy, TRUE);
+}
+
+void HSplitter::onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	SetCapture(hWnd);
+	_OldPos = -1;
+}
+
+void HSplitter::onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (GetCapture() != hWnd)
+	{
+		return;
+	}
+
+	ReleaseCapture();
+
+	if (_OldPos > 0)
+	{
+		Draw(hWnd, _OldPos);
+	}
+
+	SendMessage(hWnd, WM_SIZE, 0, 0);
+}
+
+void HSplitter::onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (GetCapture() != hWnd)
+	{
+		return;
+	}
+
+
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+
+	const int posValue = GET_X_LPARAM(lParam);
+	const int maxValue = rect.right;
+	_Percent = static_cast<double>(posValue) / static_cast<double>(maxValue);
+
+
+	if (_Percent < 0.1)
+	{
+		_Percent = 0.1;
+	}
+	else if (_Percent > 0.9)
+	{
+		_Percent = 0.9;
+	}
+
+
+	const int curValue = static_cast<int>(maxValue * _Percent);
+	if (_OldPos != curValue)
+	{
+		if (_OldPos > 0)
+		{
+			Draw(hWnd, _OldPos);
+		}
+
+		Draw(hWnd, curValue);
+		_OldPos = curValue;
+	}
+
+
+	SendMessage(hWnd, WM_SIZE, 0, 0);
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
 constexpr LPCWSTR MainFrame_ClassName = L"xMainFrame";
 
 
@@ -201,14 +521,7 @@ LRESULT MainFrame::onSize(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam
 	}
 #endif
 	{
-		int _w0;
-		int _w1;
-
-		_w0 = static_cast<int>(     (cy * _Splitter._percent) - (_Splitter._Width / 2));
-		_w1 = static_cast<int>(cy - (cy * _Splitter._percent) - (_Splitter._Width / 2));
-
-		::MoveWindow(_WidgetView->_hWnd, 0,                      0, cx, _w0, TRUE);
-		::MoveWindow(_LogView   ->_hWnd, 0, _w0 + _Splitter._Width, cx, _w1, TRUE);
+		_Splitter.UpdateLayout(_hWnd, _WidgetView->_hWnd, _LogView->_hWnd);
 	}
 
 
--- Application/AppWindow/MainFrame.hpp
+++ Application/AppWindow/MainFrame.hpp
@@ -8,103 +8,26 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-//https://github.com/kenjinote/SplitterControl/blob/master/Source.cpp
 class VSplitter
 {
 public:
 	const int _Width{ 4 };
-	double _percent = 0.8;
-	int _oldposx;
+	COLORREF  _Color;
+
+	double    _Percent;
+	int       _OldPos;
 
 public:
-	void Draw(HWND hWnd, INT x)
-	{
-		const HDC hdc = GetDC(hWnd);
-		if (hdc)
-		{
-			//const HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
-			const HBRUSH hBrush = CreateSolidBrush(RGB(248, 248, 248));
-			const HPEN hPen= CreatePen(PS_SOLID, 1, RGB(248, 248, 248));
-			if (hBrush)
-			{
-				RECT rect;
-				GetClientRect(hWnd, &rect);
-				const RECT rectSplit = 
-				{
-					0         , x - _Width / 2,
-					rect.right, x + _Width / 2
-				};
-
-				//SetROP2(hdc, R2_NOT);
-				//SetBkMode(hdc, TRANSPARENT);
-
-				const HGDIOBJ hOldBrush = SelectObject(hdc, hBrush);
-				const HGDIOBJ hOldPen = SelectObject(hdc, hPen);
-
-				Rectangle(hdc, rectSplit.left, rectSplit.top, rectSplit.right, rectSplit.bottom);
-				
-				SelectObject(hdc, hOldBrush);
-				SelectObject(hdc, hOldPen);
-
-				DeleteObject(hBrush);
-				DeleteObject(hPen);
-			}
-			ReleaseDC(hWnd, hdc);
-		}
-	}
+	VSplitter();
 
 public:
-	void onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		SetCapture(hWnd);
-		_oldposx = -1;
-	}
+	void Draw(HWND hWnd, int curPos);
+	void UpdateLayout(HWND hWnd, HWND hFirstWnd, HWND hSecondWnd);
 
-	void onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		if (GetCapture() == hWnd)
-		{
-			const int posx = GET_Y_LPARAM(lParam);
-
-
-			RECT rect;
-			
-			
-			GetClientRect(hWnd, &rect);
-			
-
-			_percent = (double)posx / (double)rect.bottom;
-
-
-			if (_percent < 0.1) _percent = 0.1;
-			else if (_percent > 0.9) _percent = 0.9;
-
-
-			const int x = (int)(rect.bottom * _percent);
-
-
-			if (_oldposx != x)
-			{
-				if (_oldposx > 0) Draw(hWnd, _oldposx);
-				Draw(hWnd, x);
-				_oldposx = x;
-			}
-
-			SendMessage(hWnd, WM_SIZE, 0, 0);
-		}
-	}
-
-	void onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		if (GetCapture() == hWnd)
-		{
-			ReleaseCapture();
-
-			if (_oldposx > 0) Draw(hWnd, _oldposx);
-
-			SendMessage(hWnd, WM_SIZE, 0, 0);
-		}
-	}
+public:
+	void onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
+	void onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
+	void onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
 };
 
 
@@ -117,79 +40,22 @@ class HSplitter
 {
 public:
 	const int _Width{ 4 };
-	double _percent = 0.5;
-	int _oldposx;
+	COLORREF  _Color;
+
+	double    _Percent;
+	int       _OldPos;
 
 public:
-	void Draw(HWND hWnd, INT x)
-	{
-		const HDC hdc = GetDC(hWnd);
-		if (hdc)
-		{
-			const HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
-			if (hBrush)
-			{
-				RECT rect;
-				GetClientRect(hWnd, &rect);
-				const RECT rectSplit = { x - _Width / 2, 0, x + _Width / 2, rect.bottom };
-				SetROP2(hdc, R2_NOT);
-				SetBkMode(hdc, TRANSPARENT);
-				const HGDIOBJ hOldBrush = SelectObject(hdc, hBrush);
-				Rectangle(hdc, rectSplit.left, rectSplit.top, rectSplit.right, rectSplit.bottom);
-				SelectObject(hdc, hOldBrush);
-				DeleteObject(hBrush);
-			}
-			ReleaseDC(hWnd, hdc);
-		}
-	}
+	HSplitter();
 
 public:
-	void onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		SetCapture(hWnd);
-		_oldposx = -1;
-	}
+	void Draw(HWND hWnd, int curPos);
+	void UpdateLayout(HWND hWnd, HWND hFirstWnd, HWND hSecondWnd);
 
-	void onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		if (GetCapture() == hWnd)
-		{
-			const int posx = GET_X_LPARAM(lParam);
-
-
-			RECT rect;
-
-
-			GetClientRect(hWnd, &rect);
-
-
-			_percent = (double)posx / (double)rect.right;
-
-
-			if (_percent < 0.1) _percent = 0.1;
-			else if (_percent > 0.9) _percent = 0.9;
-
-
-			const int x = (int)(rect.right * _percent);
-
-
-			if (_oldposx != x)
-			{
-				if (_oldposx > 0) Draw(hWnd, _oldposx);
-				Draw(hWnd, x);
-				_oldposx = x;
-			}
-		}
-	}
-
-	void onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		if (GetCapture() == hWnd)
-		{
-			ReleaseCapture();
-			SendMessage(hWnd, WM_SIZE, 0, 0);
-		}
-	}
+public:
+	void onLButtonDown(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
+	void onLButtonUp(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
+	void onMouseMove(HWND hWnd, uint32_t uMsg, WPARAM wParam, LPARAM lParam);
 };
 
 
